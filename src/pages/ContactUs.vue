4292d2593deb88051fd80ec494ace76406dd9947
deng <denglinghua@outlook.com>
2024-01-27 20:53:28 -0400

    add contact map border

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -37,8 +37,8 @@
       </q-item>
     </q-list>
   </q-card>
-  <q-card class="my-card" flat :bordered="false" style="max-width: 600px">
-    <q-card-section>
+  <q-card class="q-mt-md q-ml-md" style="max-width: 600px">
+    <q-card-section class="q-pa-sm">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2759.7807981024735!2d-63.1312319241054!3d46.23470368170146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4b5e52e0f814d261%3A0xff997adf42023187!2s155%20Queen%20St%2C%20Charlottetown%2C%20PE%20C1A%204B4!5e0!3m2!1sen!2sca!4v1706220916063!5m2!1sen!2sca"
         width="100%"
