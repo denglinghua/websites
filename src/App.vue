e5a3b64ad935036a3b22d8bd075ea3849023e303
deng <denglinghua@outlook.com>
2024-01-28 22:06:42 -0400

    composition api

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,9 @@
   <router-view />
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'App'
-})
+<script setup>
+import { useQuasar } from "quasar";
+const $q = useQuasar();
+// set status
+//$q.dark.set(true); // or false or "auto"
 </script>
-
