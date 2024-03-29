dca3618271ff83e40b8f6aaec2ad61d4eca948d9
deng <denglinghua@outlook.com>
2024-03-29 00:48:03 -0300

    zoom image

--- giftloving/src/components/ProductItem.vue
+++ giftloving/src/components/ProductItem.vue
@@ -1,7 +1,7 @@
 <template>
   <q-card class="q-ma-md p-card">
     <q-card-section class="q-pa-sm">
-      <q-img :src="`ps/${product.image}`" :ratio="1" spinner-color="primary" />
+      <q-img :src="`ps/${product.image}`" :ratio="1" spinner-color="primary" @click="zoom" class="zoomable" />
     </q-card-section>
     <q-card-section>
       <div>{{ product.description }}</div>
@@ -10,9 +10,16 @@
 </template>
 <script setup>
 const props = defineProps(["product"]);
+
+function zoom() {
+  console.log("zoom");
+}
 </script>
 <style lang="sass" scoped>
 .p-card
   width: 350px
   height: 420px
+
+.zoomable
+  cursor: zoom-in
 </style>
