70bb6a71e91ef12d48829dab987e429a329b53fd
deng <denglinghua@outlook.com>
2024-04-25 22:31:56 -0300

    update upload.sh

--- giftloving/build/build.sh
+++ giftloving/build/build.sh
@@ -1,5 +1,5 @@
 echo "Building the project..."
 quasar build
 echo "Removing .DS_Store files..."
-find ./dist -name .DS_Store -type f -delete
+find ../dist -name .DS_Store -type f -delete
 echo "Build completed successfully."
