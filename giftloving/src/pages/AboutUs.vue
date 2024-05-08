c1f6bdaaa7c5e7b833a196c93c455511df2fef6e
denglinghua <denglinghua@outlook.com>
2024-05-08 09:19:15 -0300

    shorten about us text

--- giftloving/src/pages/AboutUs.vue
+++ giftloving/src/pages/AboutUs.vue
@@ -4,27 +4,19 @@
       <q-img src="img/about.jpg" style="max-height: 50vh" />
       <q-card-section class="q-pt-md">
         <p>
-          Welcome to <b>I&M GIFTLOVING</b>, where we specialize in crafting exquisite dried and preserved collections.
-          Our passion lies in creating timeless pieces that capture the beauty of nature, from elegant basket flowers to
-          stunning bouquets and bundles.
+          Welcome to <b>I&M GIFTLOVING</b>, , where we specialize in crafting exquisite dried and preserved collections,
+          capturing nature's beauty in timeless pieces, from elegant basket flowers to stunning bouquets and bundles.
         </p>
         <p>
-          Every product you find here is meticulously designed and handcrafted by our skilled artisans. We believe in
-          the art of preservation, carefully selecting each element to ensure longevity and lasting beauty. With
-          attention to detail and a commitment to quality, we aim to bring the enchanting allure of nature into your
-          home.
+          Every product is meticulously designed and handcrafted by skilled artisans, reflecting our commitment to
+          preservation and lasting beauty.
         </p>
         <p>
-          At I&M GIFTLOVING, we take pride in our unique creations, each one reflecting our dedication to
-          craftsmanship and creativity. Whether you're searching for a statement piece for your own space or a
-          thoughtful gift for someone special, you'll discover a curated selection of handmade treasures that celebrate
-          the splendor of nature.
+          Discover our unique creations, meticulously crafted to bring the enchanting allure of nature into your home.
         </p>
         <p>
-          Thank you for choosing I&M GIFTLOVING. We invite you to <a href="/products">explore our collection</a> and
-          experience
-          the
-          timeless charm of our dried and preserved creations.
+          Thank you for choosing I&M GIFTLOVING. <a href="/products">Explore our collection and experience </a>
+          the timeless charm of our creations.
         </p>
       </q-card-section>
     </q-card>
