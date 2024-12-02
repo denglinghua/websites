904d92867497f9342ced47be938016657717de91
deng <denglinghua@outlook.com>
2024-12-02 18:11:13 -0400

    update images

--- wow/src/pages/ServicePage.vue
+++ wow/src/pages/ServicePage.vue
@@ -1,41 +1,24 @@
 <template>
   <div class="column items-center q-mb-sm">
-    <template v-for="(s, idx) in services" :key="s.title">
-      <q-card class="service-card">
-        <q-card-section :horizontal="!isSmallScreen()">
-          <q-card-section class="q-pt-xs">
-            <div class="text-h5 q-mt-sm q-mb-xs">{{ s.title }}</div>
-            <div class="text-grey">
-              {{ s.description }}
-            </div>
-          </q-card-section>
-          <q-card-section class="col-6 flex flex-center">
-            <q-img class="rounded-borders" :src="s.img" />
-          </q-card-section>
-        </q-card-section>
-      </q-card>
+    <q-card class="service-card">
+      <q-card-section class="q-pt-xs">
+        <div class="text-h5 q-mt-sm q-mb-xs">Customize</div>
+        <div class="text-grey">
+          One of our standout features is our personalized customization service. We specialize in tailoring home
+          textile products to meet your specific needs, including sewing custom logos or patterns to add a unique
+          touch to your items. We pride ourselves on offering excellent value for money combined with personalized
+          service to ensure customer satisfaction.
+        </div>
+      </q-card-section>
+      <q-card-section class="col-6 flex flex-center">
+        <q-img class="rounded-borders" src="/img/service/customize1.jpg" />
+      </q-card-section>
+    </q-card>
 
-      <q-separator class="q-mt-md" v-if="idx != services.length - 1" />
-    </template>
   </div>
 </template>
 <script setup>
 import { isSmallScreen } from "src/g";
-
-const services = [
-  {
-    title: "Customize Your Space",
-    description:
-      "Tailor your home to perfection with our bespoke customization service. From unique furniture pieces to personalized decor accents, we'll bring your vision to life, creating a space that's truly yours.",
-    img: "img/custom.jpg",
-  },
-  {
-    title: "Elevate Your Sale",
-    description:
-      "Maximize your property's potential with our professional home staging service. We'll transform your house into a captivating home that stands out in the market, helping you secure a faster sale at the best price.",
-    img: "img/staging.jpg",
-  },
-];
 </script>
 <style lang="sass" scoped>
 .service-card
