1620ab66a210d4c2456baeba49bc628c6f54b781
deng <denglinghua@outlook.com>
2024-12-27 09:41:47 -0400

    refactory class binding

--- sportelements/src/pages/AboutUs.vue
+++ sportelements/src/pages/AboutUs.vue
@@ -4,7 +4,7 @@
       <q-card-section :horizontal="!$mb" class="q-pb-none">
         <q-img src="/img/home/vmain.avif" :class="{ 'q-ml-xl': !$mb }" style="max-width: 500px; max-height: 500px" />
         <q-card-section>
-          <div class="column text-uppercase text-h2 slogan" :class="{ 'q-ml-xl': !$mb, 'q-mt-xl': !$mb }">
+          <div class="column text-uppercase text-h2 slogan" :class="$mb ? [] : ['q-ml-xl', 'q-mt-xl']">
             <div>Gear Up</div>
             <div>for</div>
             <div>Every</div>
